dd6ff5fcc2733a773e1802d5709426450abc5341
DiGureev <dianagureev@gmail.com>
2023-09-05 21:52:58 +0300

    Ex-Xp gold, ninja Day2

--- Week2/Day2/EX-Ninja/ex-ninja.py
+++ Week2/Day2/EX-Ninja/ex-ninja.py
@@ -1,39 +1,161 @@
-#Ex-3
+# Exercise 1: Formula
+# Instructions
+# Write a program that calculates and prints a value according to this given formula:
+# Q = Square root of [(2 * C * D)/H]
+# Following are the fixed values of C and H:
+# C is 50.
+# H is 30.
+# Ask the user for a comma-separated string of numbers, use each number from the user as D in the formula and return all the results
+# For example, if the user inputs: 100,150,180
+# The output should be:
 
-# Predict the output of the following code snippets:
-#     >>> 3 <= 3 < 9 → True
-#     >>> 3 == 3 == 3 → True
-#     >>> bool(0) → False
-#     >>> bool(5 == "5") → False
-#     >>> bool(4 == 4) == bool("4" == "4") → True
-#     >>> bool(bool(None)) → False
-    
-    # # x = (1 == True) → Error
-    # # y = (1 == False) → Error
-    # a = True + 4 → Error
-    # b = False + 10 → Error
+# 18,22,24
 
-    # # print("x is", x)
-    # # print("y is", y)
-    # print("a:", a)
-    # print("b:", b)
+# import math
 
-#Ex-4
+# C = 50
+# H = 50
+# user_input = input('Give me three numbers,separated by comma: ') #1,2,3
+# D = list((user_input.split(",")))
 
-# Use python to find out how many characters are in the following text, use a single line of code (beyond the establishment of your my_text variable).
+# def some_formula(item):
+#     return math.sqrt((2 * C * int(item))/H)
+# Q = list(map(some_formula,(D)))
+# print(Q)
 
-# my_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-# print(len(my_text))
+# Exercise 2 : List Of Integers
+# Instructions
+# Given a list of 10 integers to analyze. For example:
 
-#Ex-5
+#     [3, 47, 99, -80, 22, 97, 54, -23, 5, 7] 
+#     [44, 91, 8, 24, -6, 0, 56, 8, 100, 2] 
+#     [3, 21, 76, 53, 9, -82, -3, 49, 1, 76] 
+#     [18, 19, 2, 56, 33, 17, 41, -63, -82, 1]
+# Store the list of numbers in a variable.
 
-# Keep asking the user to input the longest sentence they can without the character “A”.
-# Each time a user successfully sets a new longest sentence, print a congratulations message.
+# some_numbers = [18, 19, 2, 56, 33, 17, 41, -63, -82, 1]
 
-user_choice = input("Create the longest sentence you can without 'A' character\n")
+# Print the following information:
+# a. The list of numbers – printed in a single line
 
-if "a" not in user_choice or "A" not in user_choice:
-    print("Nice try, but no")
-else:
-    print("Wow! Congrats!")
+# print(*some_numbers, sep = "\n")
+
+# b. The list of numbers – sorted in descending order (largest to smallest)
+
+# print(sorted(some_numbers))
+
+# c. The sum of all the numbers
+
+# print(sum(some_numbers))
+
+# A list containing the first and the last numbers.
+
+# other_list = [some_numbers[0], some_numbers[-1]]
+
+# print(other_list)
+
+# A list of all the numbers greater than 50.
+
+# other_list = [x for x in some_numbers if x > 50]
+# print(other_list)
+
+# A list of all the numbers smaller than 10.
+
+# other_list = [x for x in some_numbers if x < 10]
+# print(other_list)
+
+# A list of all the numbers squared – eg. for [1, 2, 3] you would print “1 4 9”.
+
+# def multiple(item):
+#     return item**2
+
+# print(list(map(multiple,some_numbers)))
+
+# The numbers without any duplicates – also print how many numbers are in the new list.
+
+# other_list = []
+
+# for number in some_numbers:
+#     number_str = str(number)
+#     if len(number_str) == len(set(number_str)):
+#         other_list.append(number)
+
+# print(other_list)
+
+# The average of all the numbers.
+
+# print(sum(some_numbers)/(len(some_numbers)))
+
+
+# The largest number.
+
+# a = max(some_numbers)
+# print(a)
+
+# 10.The smallest number.
+
+# a = min(some_numbers)
+# print(a)
+
+# 11.Bonus: Find the sum, average, largest and smallest number without using built in functions.
+# 12.Bonus: Instead of using pre-defined lists of numbers, ask the user for 10 numbers between -100 and 100. Ask the user for an integer between -100 and 100 – repeat this question 10 times. Each number should be added into a variable that you created earlier.
+
+# some_list = []
+
+# for i in range(10):
+#     a = input('Give me a number from -100 to 100')
+#     some_list.append(a)
+# print(some_list)
+
+# 13.Bonus: Instead of asking the user for 10 integers, generate 10 random integers yourself. Make sure that these random integers are between -100 and 100.
+
+# import random
+
+# some_numbers = []
+
+# for i in range(10):
+#     letter = random.randrange(-100,101)
+#     some_numbers.append(letter)
+# print(some_numbers)
+
+# 14.Bonus: Instead of always generating 10 integers, let the amount of integers also be random! Generate a random positive integer no smaller than 50.
+
+# 15.Bonus: Will the code work when the number of random numbers is not equal to 10?
+
+# Exercise 3: Working On A Paragraph
+# Find an interesting paragraph of text online. (Please keep it appropriate to the social context of our class.)
+# Paste it to your code, and store it in a variable.
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+
+# Let’s analyze the paragraph. Print out a nicely formatted message saying:
+# How many characters it contains (this one is easy…).
+print(len(text))
+
+# How many sentences it contains.
+text_sent = text.split('.')
+print(len(text_sent))
+
+# How many words it contains.
+
+text_convert = text.replace('.', ' ').replace(',',' ')
+text_word = text_convert.split(' ')
+print(len(text_word))
+
+# How many unique words it contains.
+
+unique_words = set(text_word)
+print(len(unique_words))
+
+# Bonus: How many non-whitespace characters it contains.
+text_space = text.replace(' ', '')
+print(len(text_space))
+
+# Bonus: The average amount of words per sentence in the paragraph.
+
+print(len(text_word)/len(text_sent))
+# Bonus: the amount of non-unique words in the paragraph.
+
+print((len(text_word))-len(unique_words))
+
--- Week2/Day2/Ex-XP-Gold/ex-xp-gold.py
+++ Week2/Day2/Ex-XP-Gold/ex-xp-gold.py
@@ -1,43 +1,180 @@
-#Ex-1
+# Exercise 1: Concatenate Lists
+# Instructions
+# Write code that concatenates two lists together without using the + sign.
 
-# Print the following output in one line of code:
-# Hello world
-# Hello world
-# Hello world
-# Hello world
-# I love python
-# I love python
-# I love python
-# I love python
+# list1 = [1,2,3]
+# list2 = [4,5,6]
+# for i in list2:
+#     list1.append(i)
 
-#ANSWER
+# print(list1)
 
-# a = "Hello world\n" * 4
-# b = "I love python\n" * 4
-# c = a + b
+# Exercise 2: Range Of Numbers
+# Instructions
+# Create a loop that goes from 1500 to 2500 and prints all multiples of 5 and 7.
 
-# print(c)
+# for x in range(1500,2500,1):
+#     if x % 5 == 0 or x % 7 == 0:
+#         print(x)
 
-# Ex-2
+# Exercise 3: Check The Index
+# Instructions
+# Using this variable
+# Ask a user for their name, if their name is in the names list print out the index of the first occurence of the name.
+# Example: if input is 'Cortana' we should be printing the index 1
 
-# Ask the user to input a month (1 to 12).
-# Display the season of the month received :
-# Spring runs from March (3) to May (5)
-# Summer runs from June (6) to August (8)
-# Autumn runs from September (9) to November (11)
-# Winter runs from December (12) to February (2)
+# names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
-#ANSWER
+# user_answer = input('What is your name?')
 
-# user_choice = int(input("Give me a month in number (1-12)\n"))
+# if user_answer in names:
+#     print(names.index(user_answer))
 
-# if user_choice == 12 or user_choice <= 2:
-#     print("it's winter!")
-# elif user_choice <= 5:
-#     print("it's spring!")
-# elif user_choice <= 8:
-#     print("it's summer!")
-# elif user_choice < 12:
-#     print("it's autumn!")
+
+# Exercise 4: Greatest Number
+# Instructions
+# Ask the user for 3 numbers and print the greatest number.
+#     Test Data
+#     Input the 1st number: 25
+#     Input the 2nd number: 78
+#     Input the 3rd number: 87
+
+#     The greatest number is: 87
+
+
+# user_input1 = input('Input the 1st number: ')
+# user_input2 = input('Input the 2snd number: ')
+# user_input3 = input('Input the 3rd number: ')
+
+# if int(user_input1) > int(user_input2) and int(user_input1) > int(user_input3):
+#     print(f'The greatest number is {user_input1}')
+# elif int(user_input2) > int(user_input1) and int(user_input2) > int(user_input3):
+#     print(f'The greatest number is {user_input2}')
+# elif int(user_input3) > int(user_input1) and int(user_input3) > int(user_input2):
+#     print(f'The greatest number is {user_input3}')
+# elif int(user_input1) == int(user_input2) and int(user_input2) == int(user_input3):
+#     print('The numbers are equal')
 # else:
-#     print ("Wrong number of the month")
+#     print("There is two equal numbers, I can't chose one")
+
+
+
+# Exercise 5: The Alphabet
+# Instructions
+# Create a string of all the letters in the alphabet
+# Loop over each letter and print a message that contains the letter and whether its a vowel or a consonant.
+
+# alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# for index,letter in enumerate(alphabet):
+#     if index == 0 or index == 4 or index == 8 or index == 14 or index == 20:
+#         print(f'The letter {letter} is a vowel')
+#     else:
+#         print(f'The letter {letter} is a consonant')
+
+# Exercise 6: Words And Letters
+# Instructions
+# Ask a user for 7 words, store them in a list named words.
+# Ask the user for a single character, store it in a variable called letter.
+# Loop through the words list and print the index of the first appearence of the letter variable in each word of the list.
+# If the letter doesn’t exist in one of the words, print a friendly message with the word and the letter.
+
+# user_input = input('Please, give me 7 words, separated by comma: ')
+# words = list(user_input.split(","))
+# letter = input('Plaese, give me any single character: ')
+
+# for word in words:
+#     if letter in word:
+#         print(word.index(letter))
+#     else:
+#         print(f"There is no such letter {letter} in this word: '{word}'")
+
+
+# Exercise 7:
+# Instructions
+# Create a list of numbers from one to one million and then use min() and max() to make sure your list actually starts at one and ends at one million. Use the sum() function to see how quickly Python can add a million numbers.
+
+# numbers = [i for i in range(1,1000001)]
+
+# print(numbers)
+# a = min(numbers)
+# b = max(numbers)
+
+# print(a,b)
+
+# print(sum(numbers))
+
+# Exercise 8 : List And Tuple
+# Instructions
+# Write a program which accepts a sequence of comma-separated numbers. Generate a list and a tuple which contain every number.
+# Suppose the following input is supplied to the program: 34,67,55,33,12,98
+
+# Then, the output should be:
+
+# ['34', '67', '55', '33', '12', '98']
+# ('34', '67', '55', '33', '12', '98')
+
+# user_input = input('Give me numbers, separated by comma: ')
+
+# list_numbers = list(user_input.split(","))
+# tuple_numbers = tuple(user_input.split(","))
+
+# print(list_numbers)
+# print(tuple_numbers)
+
+
+# Exercise 9 : Random Number
+# Instructions
+# Ask the user to input a number from 1 to 9 (including).
+# Get a random number between 1 and 9. Hint: random module.
+# If the user guesses the correct number print a message that says Winner.
+# If the user guesses the wrong number print a message that says better luck next time.
+
+# import random
+
+# user_input = input('Guess the number from 1 to 9, included: ')
+# letter = random.randrange(1,9)
+
+# if user_input == letter:
+#     print('Wow! You win!')
+# else:
+#     print('Better luck next time!')
+
+# Bonus: use a loop that allows the user to keep guessing until they want to quit.
+
+# import random
+# letter = random.randrange(1,9)
+
+# while True:
+#     user_input = input('Guess the number from 1 to 9, included. Or type "quit" ')
+#     print(user_input)
+#     if user_input == 'quit':
+#         break
+#     elif int(user_input) == letter:
+#         print('You won!')
+#         break
+#     else:
+#         print('Better luck next time!')
+
+
+# Bonus 2: on exiting the loop tally up and display total games won and lost.
+
+# import random
+# letter = random.randrange(1,9)
+
+# points = 0
+# losts = 0
+
+# while True:
+#     user_input = input('Guess the number from 1 to 9, included. Or type "quit" ')
+#     print(user_input)
+#     if user_input == 'quit':
+#         print(f'Your won {points} time, You losted {losts} times. Bye!')
+#         break
+#     elif int(user_input) == letter:
+#         points +=1
+#         print(f'You won! Your won {points} time, You losted {losts} times')
+#         break
+#     else:
+#         losts += 1
+#         print('Better luck next time!')
