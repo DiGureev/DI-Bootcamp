197328c6bdb4c64f7d4714c447f3dbce41d62a4f
DiGureev <dianagureev@gmail.com>
2023-09-08 18:05:01 +0300

    DC-gold Day5

---
+++ Week2/Day5/DailyChallange/DC-gold.py
@@ -0,0 +1,16 @@
+# Instructions
+# Here is a python code that generate a list of 20000 random numbers, called list_of_numbers.
+# Extend this code to guess how many couples of numbers in list_of_numbers sum to target_number.
+
+# import random
+
+# list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+# target_number   = 3728
+
+# count_couples = 0
+
+# for index, num in enumerate(list_of_numbers[::-1]):
+#     if num + list_of_numbers[index-1] == 3728:
+#         count_couples +=1
+# print(count_couples)
+
