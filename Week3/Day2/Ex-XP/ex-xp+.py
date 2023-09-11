d2c6df16aee658af0527311002ee4f11aed996f0
DiGureev <dianagureev@gmail.com>
2023-09-11 21:20:10 +0300

    Ex-xp+

--- Week3/Day2/Ex-XP/ex-xp+.py
+++ Week3/Day2/Ex-XP/ex-xp+.py
@@ -1,63 +1,88 @@
-# 🌟 Exercise 1 : Pets
+# Exercise 1 : Family
 # Instructions
-# Using this code:
+# Create a class called Family and implement the following attributes:
 
-# class Pets():
-#     def __init__(self, animals):
-#         self.animals = animals
+# 2. Implement the following methods:
 
-#     def walk(self):
-#         for animal in self.animals:
-#             print(animal.walk())
+# born: adds a child to the members list (use **kwargs), don’t forget to print a message congratulating the family.
+# is_18: takes the name of a family member as a parameter and returns True if they are over 18 and False if not.
+# family_presentation: a method that prints the family’s last name and all the members’ first name.
 
-# class Cat():
-#     is_lazy = True
+# class Family:
 
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+#     def __init__(self):
+#         self.last_name = 'Smith'
+#         self.members = [
+#         {'name':'Michael','age':35,'gender':'Male','is_child':False, 'power': 'fly','incredible_name':'MikeFly'},
+#         {'name':'Sarah','age':32,'gender':'Female','is_child':False, 'power': 'read minds','incredible_name':'SuperWoman'},
+#         ]
+    
+#     def born(self, **child):
+#         new_dict = {}
+#         for key, value in child.items():
+#             new_dict[key] = value
+#         self.members.append(new_dict)
+#         print('Congrats!')
 
-#     def walk(self):
-#         return f'{self.name} is just walking around'
+#     def is_18(self, name):
+#         for i in self.members:
+#             if i['name'] == name:
+#                 print(i['is_child'])
 
-# class Bengal(Cat):
-#     def sing(self, sounds):
-#         return f'{sounds}'
+#     def family_presentation(self):
+#         print(f"The family's last name is {self.last_name}.There are:")
+#         for i in self.members:
+#             print(i['name'])
 
-# class Chartreux(Cat):
-#     def sing(self, sounds):
-#         return f'{sounds}'
-
-
-# Create another cat breed named Siamese which inherits from the Cat class.
-# Create a list called all_cats, which holds three cat instances : one Bengal, one Chartreux and one Siamese.
-# Those three cats are Sara’s pets. Create a variable called sara_pets which value is an instance of the Pet class, and pass the variable all_cats to the new instance.
-# Take all the cats for a walk, use the walk method.
-
-
-# 🌟 Exercise 2 : Dogs
+# Exercise 2 : TheIncredibles Family
 # Instructions
-# Create a class called Dog with the following attributes name, age, weight.
-# Implement the following methods in the Dog class:
-# bark: returns a string which states: “<dog_name> is barking”.
-# run_speed: returns the dogs running speed (weight/age*10).
-# fight : takes a parameter which value is another Dog instance, called other_dog. This method returns a string stating which dog won the fight. The winner should be the dog with the higher run_speed x weight.
+# Create a class called TheIncredibles. This class should inherit from the Family class:
+# This is no random family they are an incredible family, therefore we need to add the following keys to our dictionaries: power and incredible_name.
 
-# Create 3 dogs and run them through your class.
+# Initial members data:
+
+# Add a method called use_power, this method should print the power of a member only if they are over 18 years old. 
+# If not raise an exception (look up exceptions) which stated they are not over 18 years old.
+
+# class TheIncredibles(Family):
+    
+#     def __init__(self):
+#         super().__init__()
+    
+#     def use_power(self):
+#         for i in self.members:
+#             try:
+#                 i['is_child'] == False
+#                 print(f"{i['name']} has a power: {i['power']}")
+#             except:
+#                 print(f"{i['name']} not over 18 years old")
+
+#     def incredible_presentation(self):
+#         print(super().family_presentation())
+#         for i in self.members:
+#             print(f"{i['name']}'s increadible name is {i['incredible_name']} and super-power is {i['power']}!")
+            
+
+# lize = TheIncredibles()
+
+# lize.born(name = 'Jack', age = 0, gender = 'Female', is_child = True, incredible_name = 'Lol', power = 'Unknown power' )
+# lize.is_18('Jack')
+# lize.family_presentation()          
+
+# lize.use_power()
+# lize.incredible_presentation()
 
 
-# 🌟 Exercise 3 : Dogs Domesticated
-# Instructions
-# Create a new python file and import your Dog class from the previous exercise.
-# In the new python file, create a class named PetDog that inherits from Dog.
-# Add an attribute called trained to the __init__ method, this attribute is a boolean and the value should be False by default.
-# Add the following methods:
-# train: prints the output of bark and switches the trained boolean to True
 
-# play: takes a parameter which value is a few names of other Dog instances (use *args). The method should print the following string: “dog_names all play together”.
+# Add a method called incredible_presentation which :
 
-# do_a_trick: If the dog is trained the method should print one of the following sentences at random:
-# “dog_name does a barrel roll”.
-# “dog_name stands on his back legs”.
-# “dog_name shakes your hand”.
-# “dog_name plays dead”.
+# Prints the family’s last name and all the members’ first name (ie. use the super() function, to call the family_presentation method)
+# Prints all the members’ incredible name and power.
+
+# Call the incredible_presentation method.
+
+
+# Use the born method inherited from the Family class to add Baby Jack with the following power: “Unknown Power”.
+
+
+# Call the incredible_presentation method again.
