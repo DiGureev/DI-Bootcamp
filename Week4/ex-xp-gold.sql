f9577b2bdd65c26c3040bb178c3810a7b865fe02
DiGureev <dianagureev@gmail.com>
2023-09-18 16:30:01 +0300

    ex-gold

---
+++ Week4/ex-xp-gold.sql
@@ -0,0 +1,21 @@
+-- create table students (
+-- id serial primary key,
+-- 	first_name varchar,
+-- 	last_name varchar,
+-- 	birth_date date
+-- )
+-- ;
+
+-- insert into students (first_name, last_name, birth_date)
+-- values
+-- ('Marc','Benichou', '1991-11-02'),
+-- ('Yoan','Cohen', '2010-12-03'),
+-- ('Lea','Benichou', '1987-07-27'),
+-- ('Amelia ','Dux', '1996-04-07'),
+-- ('David ','Grez', '2003-06-14'),
+-- ('Omer ','Simpson', '1980-10-03')
+-- ;
+
+-- select * from students where id in (1,2,3,4) order by last_name;
+-- select max(birth_date) from students;
+-- select * from students where id<=5 and id > 2;
