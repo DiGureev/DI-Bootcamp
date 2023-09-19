4d32ba5d31f3dd0c7f0abfdafa9de7ea29fb8069
DiGureev <dianagureev@gmail.com>
2023-09-19 20:36:01 +0300

    ex-xp-ninja

---
+++ Week4/Day2/Ex-xp-ninja/ex-xp-ninja.sql
@@ -0,0 +1,9 @@
+-- select * from customers;
+
+-- select first_name, last_name from customers order by last_name desc limit 2;
+
+-- delete from customers where first_name = 'Scott';
+
+-- select * from purchases 
+-- right join customers
+-- on purchases.customer_id = customers.id;
