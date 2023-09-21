8c1d2f89bce6a7bddb049064153e07af8d9736f8
DiGureev <dianagureev@gmail.com>
2023-09-21 22:17:14 +0300

    Weather app mini-project

--- Week3/Day5/Weather_app/Weather.py
+++ Week3/Day5/Weather_app/Weather.py
@@ -1,25 +1,52 @@
-# Get the current weather in Tel Aviv.
-# Get current wind info of Tel Aviv.
-# Get today’s sunrise and sunset times of Tel Aviv.
-# Display all these information in a user friendly way.
 
-import requests
-lat = 32.08
-lon = 34.78
-response = requests.get('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=aeca4dfe96186e755db82cb454a42d70')
-# response = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=aeca4dfe96186e755db82cb454a42d70")
-print(response.status_code)
+from pyowm.owm import OWM
+from pyowm.utils import timestamps
+import datetime
+
+def weather_today(location):
+    owm = OWM('aeca4dfe96186e755db82cb454a42d70')
+    mgr = owm.weather_manager()
+    place = location
+    try:
+        observation = mgr.weather_at_place(place)  # the observation object is a box containing a weather object
+        weather = observation.weather
+        
+        temp_dict_celsius = weather.temperature('celsius')
+        wind_dict_in_meters_per_sec = observation.weather.wind()   
+        sunrise_date = weather.sunrise_time(timeformat='date')
+        sunrset_date = weather.sunset_time(timeformat='date')
+
+        print(f"Hi! Temprature in {place} today: {str(temp_dict_celsius['temp'])}, feels like: {str(temp_dict_celsius['feels_like'])}, the wind is: {str(wind_dict_in_meters_per_sec)}, sunrise at {sunrise_date}, sunset at: {sunrset_date}")
+
+    except Exception as e:
+        print(f'No such location {e}')
 
 
-# Recreate these steps, but this time, ask the user for a location (display the information in a user friendly way).
-# Instead of working with the name of the city, retrieve the id of the city.
-# Check out the documentation section : “Identifying cities and places via city IDs”.
+# weather_today('Tashkent, UZ')
 
-# Retrieve weather forecasts : The OpenWeatherMap free tier gives you access to 5 day forecasts. The forecasts contain the weather data in three-hour intervals.
-# The methods for retrieving the forecast are:
-# forecast_at_place('Los Angeles, US', '3h')
-# forecast_at_id(5391959, '3h')
-# forecast_at_coords(lat=37.774929, lon=-122.419418, interval='3h')
-# Forecasts are useful if you want to know what the weather conditions will be throughout the day/week.
+def get_forecast(location):
+    owm = OWM('aeca4dfe96186e755db82cb454a42d70')
+    mgr = owm.weather_manager()
+    place = location
+    three_h_forecast = mgr.forecast_at_place(place, '3h').forecast
+    nr_of_weathers = len(three_h_forecast)
+    print(f'Here is the forecast in {place} for next 5 days:')
+    forecast_list = []
+    for weather in three_h_forecast:
+        a = f"{weather.reference_time('iso')[0:11]} {weather.status}"
+        if a not in forecast_list:
+            forecast_list.append(a)
+    print('\n'.join(forecast_list))
 
-# Use this API to retrieve the Air Pollution in a specific city.
+# get_forecast('Tashkent, UZ')
+
+def pollution(lat, lon):
+    owm = OWM('aeca4dfe96186e755db82cb454a42d70')
+    mgr = owm.airpollution_manager()
+    coi = mgr.coindex_around_coords(lat, lon) #"coindex_around_coords" is deprecated
+    print(coi)
+    # print(mgr.__dir__())
+
+pollution(32.0853, 34.7818) #Unable to find the resource
+
+
