17bdff494e0d7dee0ddaad45e20196e50bb9d80d
DiGureev <dianagureev@gmail.com>
2023-11-23 15:48:56 +0200

    TaskManager Day3

--- Week10/Day3/task/src/Components/Todo.js
+++ Week10/Day3/task/src/Components/Todo.js
@@ -1,13 +1,21 @@
 import { useReducer, useState } from "react";
 
 function todoReducer(state, action) {
+    let newState = [...state]
     switch (action.type) {
       case "ADD_TODO":
         return [...state, { id: Date.now(), text: action.text }];
       case "REMOVE_TODO":
         return state.filter((todo) => todo.id !== action.id);
       case "EDIT_TODO":
-      return state.filter((todo) => todo.id == action.id);
+      let arr = [...state]
+      let inx = arr.findIndex((todo) => todo.id == action.id)
+      let obj = {...arr[inx]}
+      obj.text = action.text
+      arr[inx]=  obj
+      return arr
+      case "FILTER_TASKS":
+        return state.filter((todo) => todo.text.toLowerCase().includes(action.query.toLowerCase()))
       default:
         return state;
     }
@@ -15,9 +23,9 @@ function todoReducer(state, action) {
 
 export function TodoList() {
     const [todos, dispatch] = useReducer(todoReducer, []);
-    
     const [todoText, setTodoText] = useState("");
     const [display, setDisplay] = useState("none")
+    const [query, setQuery] = useState("");
   
     const handleAddTodo = () => {
       if (todoText.trim() === "") return;
@@ -27,19 +35,31 @@ export function TodoList() {
   
     const handleRemoveTodo = (id) => {
       dispatch({ type: "REMOVE_TODO", id });
+      
     };
 
     const handleEditTodo = (id) => {
-      dispatch({ type: "EDIT_TODO", id });
+      dispatch({ type: "EDIT_TODO", id, text: todoText });
+      setDisplay('none')
+    }
+
+    const handleFilterTodo = () => {
+      if (query != ''){
+        dispatch({ type: "FILTER_TASKS", query: query });
+      }       
     }
   
     return (
       <div>
         <h1>Todo List</h1>
         <input
+        type="text"
+        placeholder="Search todo"
+        onChange={(e)=>setQuery(e.target.value)}/>
+        <button onClick={handleFilterTodo}>Search</button>
+        <input
           type="text"
           placeholder="Add a new todo"
-          value={todoText}
           onChange={(e) => setTodoText(e.target.value)}
         />
         <button onClick={handleAddTodo}>Add Todo</button>
@@ -49,10 +69,9 @@ export function TodoList() {
               {todo.text}
               <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
               <button onClick={() => setDisplay('')}>Edit</button>
-              <div style={{display: {display}}}>
+              <div style={{display: display}}>
               <input
                   type="text"
-                  value={todoText}
                   onChange={(e) => setTodoText(e.target.value)}
                 />
                 <button onClick={()=> handleEditTodo(todo.id)}>Done</button>
@@ -64,3 +83,6 @@ export function TodoList() {
       </div>
     );
   }
+
+
+export default TodoList
